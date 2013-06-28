eda03b5b6c76f20243f257bb6f8f736a73e6e6cf
Albert Toledo <toledoalbert@gmail.com>
2013-06-27 20:53:08 -0400

    xstreamTest with a main method created to test functionality of XStream.(Worksfine.)

--- src/org/tekkotsu/api/xstreamTest.java
+++ src/org/tekkotsu/api/xstreamTest.java
@@ -1,17 +1,48 @@
 package org.tekkotsu.api;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import com.thoughtworks.xstream.*;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
 public class xstreamTest {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException {
 		
-		NodeClass soundNode = new NodeClass();
+		//Create conents of soundnode class
+		Parameter musicFile = new Parameter("music file", "music.wav");
+		ConstructorCall soundNodeConstructor = new ConstructorCall("soundNodeConstructor");
+		soundNodeConstructor.addParameter(musicFile);
+		System.out.println("Components of soundNode created.");
+		
+		NodeClass soundNode = new NodeClass("SoundNode", soundNodeConstructor);
+		System.out.println("SoundNode object created.");
+		
+		
 		XStream xstream = new XStream(new DomDriver());
-		xstream.toXML(
+		try {
+			//Create file to write
+			FileOutputStream file = new FileOutputStream("Nodes.xml");
+			System.out.println("Nodes.xml created.");
+			
+			//Write xml of soundNode to file.
+			xstream.toXML(soundNode, file);
+			System.out.println("soundnode written to Nodes.xml");
+			
+			
+			//Close file
+			file.close();
+			System.out.println("Node.xml closed.");
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
+	
+	
 
 }
