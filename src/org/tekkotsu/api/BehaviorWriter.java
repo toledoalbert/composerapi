aa15429ba5e0ecd3093a34f65f18879aa47b41c3
Albert Toledo <toledoalbert@gmail.com>
2013-06-28 16:42:57 -0400

    getFSM method now prints compilable code. Addition of getTransitions and getNodes methods to BehaviorWriter.

--- src/org/tekkotsu/api/BehaviorWriter.java
+++ src/org/tekkotsu/api/BehaviorWriter.java
@@ -9,6 +9,7 @@ public class BehaviorWriter {
 	private String registerCall;
 	private String include;
 	private String comment;
+	private SetupMachine setup;
 	
 	//Constructor
 	public BehaviorWriter(NodeClass nodeClass){
@@ -17,6 +18,7 @@ public class BehaviorWriter {
 		this.registerCall = "\n\t}\n}\n\nREGISTER_BEHAVIOR(" + this.nodeClass.getName() + ");";
 		this.include = "#include \"Behaviors/StateMachine.h\" \n\n";
 		this.comment = "//" + nodeClass.getName() + "Behavior\n";
+		this.setup = nodeClass.getSetupMachine();
 	}
 	
 	//Method to get the fsm content as string.
@@ -24,7 +26,7 @@ public class BehaviorWriter {
 		
 		//String for class header.
 		String classHeader = "$nodeclass " + nodeClass.getName() + " : " + "StateNode {\n";
-		String setupHeader = "\t$setupmachine {\n";
+		String setupHeader = "\t$setupmachine {\n\n";
 		
 		String meat = getMeat();
 		
@@ -40,86 +42,163 @@ public class BehaviorWriter {
 		this.comment = comment;
 	}
 	
-	//Method to get meat.
-	public String getMeat(){
+	//Method to get node declarations.
+	public String getNodes(){
 		
-		String meat = "";
+		//Create string to return
+		String nodes = "";
 		
-		SetupMachine mySetup = nodeClass.getSetupMachine();
-		
-		ArrayList<TransitionInstance> trans = mySetup.getTransitions();
-		ArrayList<NodeInstance> nodes = mySetup.getNodes();
+		//Create list of nodeinstance objects from setup object.
+		ArrayList<NodeInstance> nodeList = this.setup.getNodes();
 		
 		//For every node in the setupmachine
-		for(int i = 0; i < nodes.size(); i++){
-			NodeInstance current = nodes.get(i);
-			meat += "\t\t" + current.getLabel() + ": " + current.getType().getConstructor().getName();
+		for(int i = 0; i < nodeList.size(); i++){
+			
+			//Get the current node instance object.
+			NodeInstance current = nodeList.get(i);
+			
+			//Print its label: constructorname
+			nodes += "\t\t" + current.getLabel() + ": " + current.getType().getConstructor().getName();
 			
 			//if any parameters
 			if(current.getNumOfParameters() > 0){
-				meat += "(";
+				
+				//open parantheses
+				nodes += "(";
 				
 				//for every parameter in the constructor
 				for(int j = 0; j < current.getNumOfParameters(); j++){
-					meat +=  current.getParameters().get(j).getValue() + ", ";
+					
+					//get the value of parameter and add to the string
+					nodes +=  current.getParameters().get(j).getValue();
+					
+					if(j != current.getNumOfParameters()-1){
+						nodes += ", ";
+					}
+					
 				}
 				
-				meat += ")\n";
+				//close paranteses of the constructor
+				nodes += ")\n";
 				
 			}
 		}
 		
-		meat += "\n";
+		return nodes;
+	}
+	
+	
+	//Method to get the transitions
+	public String getTransitions(){
 		
-		//for everynode again after initialized
-		for(int i = 0; i < nodes.size(); i++){
-			NodeInstance current = nodes.get(i);
+		//Create string to return
+		String transitions = "";
+		
+		//Create transition instance objects from the setup object.
+		ArrayList<TransitionInstance> transList = this.setup.getTransitions();
+		
+		//for every transition initialized
+		for(int i = 0; i < transList.size(); i++){
 			
-			//for every outgoing transition
-			for(int j = 0; j < current.getNumOfOutTrans(); j++){
-			ConstructorCall currentTransitionConstructor = current.getOutTrans().get(j).getType().getConstructor();
-			meat += "\t\t" + current.getLabel() + " =" + currentTransitionConstructor.getName();
+			transitions += "\t\t";
+			
+			//Get current transition instance
+			TransitionInstance current = transList.get(i);
+			
+			//Get sources and targets
+			ArrayList<NodeInstance> sources = current.getSources();
+			ArrayList<NodeInstance> targets = current.getTargets();
+			
+			//if there are multiple sources
+			if(current.getNumOfSources() > 1){
+				
+				//open curly braces to put  labels of nodes in.
+				transitions += "{";
+				
+				//for every source
+				for(int j = 0; j < current.getNumOfSources(); j++){
+					
+					transitions += sources.get(j).getLabel() + ", ";
+					
+				}
+				
+				//close curly braces
+				transitions += "}";
+				
+			}else{
+				
+				//print label of the source
+				transitions += current.getSources().get(0).getLabel() + " ";
+				
+			}
+			
+			//get transition constructor.
+			ConstructorCall currentTransitionConstructor = current.getType().getConstructor();
+			
+			//Get current transition constructor name
+			transitions += "=" + currentTransitionConstructor.getName();
 			
 			//if current transition constructor has any parameters
 			if(currentTransitionConstructor.getParameters().size() > 0){
-				meat += "(";
+				transitions += "(";
 				
 				//for every parameter of transition.
 				for(int h = 0; h < currentTransitionConstructor.getParameters().size(); h++){
-					meat +=  currentTransitionConstructor.getParameters().get(h).getValue() + ", ";
+					transitions +=  currentTransitionConstructor.getParameters().get(h).getValue();
+					if(h != currentTransitionConstructor.getParameters().size()-1){
+						transitions += ", ";
+					}
 				}
-				meat += ")";
+				transitions += ")";
 			}
 			
-			meat += "=>\n";
+			transitions += "=> ";
 			
-			}
-			
-			//for every incoming transition
-			for(int j = 0; j < current.getNumOfInTrans(); j++){
-			ConstructorCall currentTransitionConstructor = current.getInTrans().get(j).getType().getConstructor();
-			
-			
-			meat += "\t\t" + " =" + currentTransitionConstructor.getName();
-			
-			//if current transition constructor has any parameters
-			if(currentTransitionConstructor.getParameters().size() > 0){
-				meat += "(";
+			//if there are multiple targets
+			if(current.getNumOfTargets() > 1){
 				
-				//for every parameter of transition.
-				for(int h = 0; h < currentTransitionConstructor.getParameters().size(); h++){
-					meat +=  currentTransitionConstructor.getParameters().get(h).getValue() + ", ";
+				//open curly braces to put  labels of nodes in.
+				transitions += "{";
+				
+				//for every source
+				for(int j = 0; j < current.getNumOfTargets(); j++){
+					
+					//print the label
+					transitions += targets.get(j).getLabel();
+					
+					if(j != current.getNumOfTargets()-1){
+						transitions += ", ";
+					}
+					
 				}
-				meat += ")";
+				
+				//close curly braces
+				transitions += "}";
+				
+			}else{
+				
+				//print label of the target
+				transitions += current.getTargets().get(0).getLabel() + " ";
+				
 			}
-
-			meat += "=> " + current.getLabel() + "\n";
+			
+			//go to the next line
+			transitions += "\n";
 			
 			}
-			
-		}
 		
+		//return the transitions
+		return transitions;
 		
+	}
+	
+	
+	//Method to get meat.
+	public String getMeat(){
+		
+		//Add string to create the meat.
+		String meat = getNodes() + "\n\n" + getTransitions();
+	
 		
 		return meat;
 		
